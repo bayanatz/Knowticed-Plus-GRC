89bbb788ed1ddc628feb150bd45b610b5ec90df8
Mohamed Abdelkhalek <58604668+mohamed296@users.noreply.github.com>
2026-07-28 13:39:50 +0300

    feat(grc): add Approval Firestore data source

---
+++ lib/features/grc/approval/data/data_source/approval_data_source.dart
@@ -0,0 +1,17 @@
+import 'package:demo_app/features/grc/approval/data/models/approval_model.dart';
+
+abstract class ApprovalDataSource {
+  Future<ApprovalModel?> get(String id, {required String moduleId});
+
+  Future<List<ApprovalModel>> getAll({required String moduleId});
+
+  Future<ApprovalModel> create(
+    ApprovalModel model, {
+    required String moduleId,
+  });
+
+  Future<ApprovalModel> update(
+    ApprovalModel model, {
+    required String moduleId,
+  });
+}
